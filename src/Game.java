b07512790effa67435083853521bb39d8282622b
Andreea <badeaioana16@gmail.com>
2023-12-27 12:56:06 +0200

    Added Basic Functionality

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,20 @@
+import java.nio.file.attribute.UserPrincipalLookupService;
+import java.util.Scanner;
+
 public class Game {
     public static void main(String[] args) {
 
+        int hiddenNumber = 32;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Guess The number");
+        int guessedNumber = sc.nextInt();
+
+        if (guessedNumber > hiddenNumber) {
+            System.out.println("Less");
+        } else if (guessedNumber < hiddenNumber) {
+            System.out.println("More");
+        } else {
+            System.out.println("You got it right!");
+        }
     }
 }
