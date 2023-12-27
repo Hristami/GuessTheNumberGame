90e35d78967b78a813d08113aa050410a4b3f4a3
Andreea <badeaioana16@gmail.com>
2023-12-27 13:14:06 +0200

    Added attempts functionality

--- src/Game.java
+++ src/Game.java
@@ -7,17 +7,19 @@ public class Game {
         Random rnd = new Random();
         int hiddenNumber = rnd.nextInt(100);
         Scanner sc = new Scanner(System.in);
+        int attemptedNumber = 0;
 
-        while (true){
+        while (true) {
             System.out.println("Guess The number");
             int guessedNumber = sc.nextInt();
+            attemptedNumber++;
 
             if (guessedNumber > hiddenNumber) {
                 System.out.println("Less");
             } else if (guessedNumber < hiddenNumber) {
                 System.out.println("More");
             } else {
-                System.out.println("You got it right!");
+                System.out.println("You got it right! You used " + attemptedNumber + " attempts");
                 break;
             }
         }
